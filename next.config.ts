26f8f8fbcbdad0f120fd1104bb78ea00ac42c0b5
galihpikun <galihrakha003@gmail.com>
2026-05-18 13:41:49 +0700

    change: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "export",
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
